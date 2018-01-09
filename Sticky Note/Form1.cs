29ed6405e6375b1941bb96dabbdd443a632c06d0
quangaming2929 <33594017+quangaming2929@users.noreply.github.com>
2018-01-09 17:17:33 +0700

    Add Lib

--- Sticky Note/Form1.cs
+++ Sticky Note/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Sticky_Note
 {
     public partial class Form1 : Form
